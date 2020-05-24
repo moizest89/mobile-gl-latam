079a99598dcbac247c78492fe42811cb06cdb3c9
moizest89 <moises.gilberto@gmail.com>
2020-05-24 13:22:33 -0400

    new: create basic view

--- app/src/main/java/com/moizest89/mobile_gl_latam/ui/MainActivity.kt
+++ app/src/main/java/com/moizest89/mobile_gl_latam/ui/MainActivity.kt
@@ -1,10 +1,10 @@
-package com.moizest89.mobile_gl_latam
+package com.moizest89.mobile_gl_latam.ui
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.moizest89.mobile_gl_latam.R
 
 import kotlinx.android.synthetic.main.activity_main.*
 
