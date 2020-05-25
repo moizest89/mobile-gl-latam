e5126bc2979372e369619ce1a253b36c999d294d
moizest89 <moises.gilberto@gmail.com>
2020-05-25 00:01:20 -0400

    new: splash view

---
+++ app/src/main/java/com/moizest89/mobile_gl_latam/presentation/splash/SplashActivity.kt
@@ -0,0 +1,45 @@
+package com.moizest89.mobile_gl_latam.presentation.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.Window
+import android.view.WindowManager
+import com.moizest89.mobile_gl_latam.R
+import com.moizest89.mobile_gl_latam.presentation.main.MainActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        makeFullScreen()
+        setContentView(R.layout.activity_splash)
+        // Using a handler to delay loading the MainActivity
+        Handler().postDelayed({
+
+            // Start activity
+            startActivity(Intent(this, MainActivity::class.java))
+
+            // Animate the loading of new activity
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+
+            // Close this activity
+            finish()
+
+        }, 3000)
+    }
+
+    private fun makeFullScreen() {
+        // Remove Title
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        // Make Fullscreen
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
+        // Hide the toolbar
+        supportActionBar?.hide()
+    }
+
+}
